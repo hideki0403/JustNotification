a916b10b06df68388c785080e5a6a0163c3c2c57
yukineko <hideki030403@gmail.com>
2021-09-02 12:54:27 +0900

    [Fix] Disable multi instance

--- JustNotification/Sources/Program.cs
+++ JustNotification/Sources/Program.cs
@@ -11,20 +11,58 @@ namespace JustNotification
         [STAThread]
         static void Main()
         {
-            // Initialize
-            Init();
+            string appName = "JustNotification";
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, appName);
 
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            new MainWindow();
-            Application.Run();
+            bool hasHandle = false;
+            try
+            {
+                try
+                {
+                    hasHandle = mutex.WaitOne(0, false);
+                }
+                catch (System.Threading.AbandonedMutexException)
+                {
+                    hasHandle = true;
+                }
+                if (hasHandle == false)
+                {
+                    // 多重起動判定
+
+                    // SteamVRからの起動であればメッセージは表示しない
+                    if(Array.IndexOf(Environment.GetCommandLineArgs(), "--steamvr") == -1)
+                    {
+                        MessageBox.Show("既に起動されているため、JustNotificationを起動できませんでした。", "起動エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    return;
+                }
+
+                // Initialize
+                Init();
+
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                new MainWindow();
+                Application.Run();
+            }
+            finally
+            {
+                if (hasHandle)
+                {
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+
         }
 
         static void Init()
         {
             Notification.Init();
             SteamVR.Init();
+            OverlayHandler.Init();
         }
     }
 }
